d53d73749999f7c1ee41c3801e607eea3c668016
Aleksandr Shishkin <kedermail@gmail.com>
2026-05-25 15:35:46 -0500

    Prophet fix

--- R_Code/prophet_fit.R
+++ R_Code/prophet_fit.R
@@ -8,66 +8,67 @@ fit_prophet <- function(cut_date, data, state) {
   train_data <- data %>% filter(date < cut_date) %>%
     # mutate(cap = max(hosp), floor = 0) %>%
     rename(ds = date, y = hosp_transformed)
-  model <- prophet(train_data, growth = "linear", 
+  model <- prophet(train_data, growth = "linear",
                    changepoint.prior.scale = 0.01,
-                   yearly.seasonality = "auto", 
+                   yearly.seasonality = "auto",
                    mcmc.samples = 1000)
   n = n_distinct(data %>% filter(date >= cut_date) %>% pull(date))
   future <- make_future_dataframe(model, periods = n, freq = "week") #%>%
   # mutate(floor = 0, cap = max(train_data$cap))
   forecasted <- predict(model, future) %>%
     dplyr::select(date = ds, fitted = yhat)
-  
-  train_data <- train_data %>% 
+
+  train_data <- train_data %>%
     dplyr::select(date = ds, hosp_transformed = y) %>%
     left_join(forecasted)
-  
-  forecasted <- forecasted %>% 
+
+  forecasted <- forecasted %>%
     inner_join(data %>% filter(date >= cut_date)) %>%
     dplyr::select(date, hosp_transformed, fitted) %>%
-    mutate(hosp = exp(hosp_transformed) - 1, 
+    mutate(hosp = exp(hosp_transformed) - 1,
            `Point Forecast` = exp(fitted) - 1)
-  
+
   train_data <- train_data %>%
     bind_rows(forecasted) %>%
-    mutate(state = state, cut_date = cut_date, 
+    mutate(state = state, cut_date = cut_date,
            hosp = exp(hosp_transformed - 1)) %>%
     dplyr::select(state, cut_date, date, hosp, everything())
   return(train_data)
 }
 
 # Functions for multivariate prophet - must `add_regressor` for each specific
-# model so combine common attirbutes at start (fit_prophet_reg_start) and 
+# model so combine common attirbutes at start (fit_prophet_reg_start) and
 # end (fit_prophet_reg_end)
 fit_prophet_reg_start <- function(cut_date, data, state){
   train_data <- data %>% filter(date < cut_date) %>%
     # mutate(cap = max(hosp), floor = 0) %>%
     rename(ds = date, y = hosp_transformed)
-  model <- prophet(growth = "linear", 
+  model <- prophet(growth = "linear",
                    changepoint.prior.scale = 0.01,
-                   yearly.seasonality = "auto", 
+                   yearly.seasonality = "auto",
                    mcmc.samples = 2000)
   return(list("train_data" = train_data, "model" = model))
 }
-fit_prophet_reg_end <- function(model, future, train_data, cut_date, state){
+fit_prophet_reg_end <- function(model, future, train_data, cut_date, state, data){
   forecasted <- predict(model, future) %>%
     dplyr::select(date = ds, fitted = yhat)
-  
-  train_data <- train_data %>% 
+
+  train_data <- train_data %>%
     dplyr::select(date = ds, hosp_transformed = y) %>%
     left_join(forecasted)
-  
-  forecasted <- forecasted %>% 
+
+  forecasted <- forecasted %>%
     inner_join(data %>% filter(date >= cut_date)) %>%
     dplyr::select(date, hosp_transformed, fitted) %>%
-    mutate(hosp = exp(hosp_transformed) - 1, 
+    mutate(hosp = exp(hosp_transformed) - 1,
            `Point Forecast` = exp(fitted) - 1)
-  
+
   train_data <- train_data %>%
     bind_rows(forecasted) %>%
-    mutate(state = state, cut_date = cut_date, 
+    mutate(state = state, cut_date = cut_date,
            hosp = exp(hosp_transformed - 1)) %>%
     dplyr::select(state, cut_date, date, hosp, everything())
+  print(train_data)
   return(train_data)
 }
 # ED visit only
@@ -78,9 +79,9 @@ fit_prophet_ed <- function(cut_date, data, state) {
   model = add_regressor(model, 'ed_transformed')
   model = fit.prophet(model, train_data)
   n = n_distinct(data %>% filter(date >= cut_date) %>% pull(date))
-  future <- make_future_dataframe(model, periods = n, freq = "week") %>% 
+  future <- make_future_dataframe(model, periods = n, freq = "week") %>%
     inner_join(data %>% select(ds = date, ed_transformed))
-  return(fit_prophet_reg_end(model, future, train_data, cut_date, state))
+  return(fit_prophet_reg_end(model, future, train_data, cut_date, state, data))
 }
 
 fit_prophet_ed_count <- function(cut_date, data, state) {
@@ -92,10 +93,10 @@ fit_prophet_ed_count <- function(cut_date, data, state) {
   model = add_regressor(model, 'percent_transformed')
   model = fit.prophet(model, train_data)
   n = n_distinct(data %>% filter(date >= cut_date) %>% pull(date))
-  future <- make_future_dataframe(model, periods = n, freq = "week") %>% 
-    inner_join(data %>% select(ds = date, ed_transformed, count_transformed, 
+  future <- make_future_dataframe(model, periods = n, freq = "week") %>%
+    inner_join(data %>% select(ds = date, ed_transformed, count_transformed,
                                percent_transformed))
-  return(fit_prophet_reg_end(model, future, train_data, cut_date, state))
+  return(fit_prophet_reg_end(model, future, train_data, cut_date, state, data))
 }
 
 fit_prophet_ww <- function(cut_date, data, state) {
@@ -105,9 +106,9 @@ fit_prophet_ww <- function(cut_date, data, state) {
   model = add_regressor(model, 'raw_pct_transformed')
   model = fit.prophet(model, train_data)
   n = n_distinct(data %>% filter(date >= cut_date) %>% pull(date))
-  future <- make_future_dataframe(model, periods = n, freq = "week") %>% 
+  future <- make_future_dataframe(model, periods = n, freq = "week") %>%
     inner_join(data %>% select(ds = date, raw_pct_transformed))
-  return(fit_prophet_reg_end(model, future, train_data, cut_date, state))
+  return(fit_prophet_reg_end(model, future, train_data, cut_date, state, data))
 }
 
 fit_prophet_ww_all <- function(cut_date, data, state) {
@@ -119,10 +120,10 @@ fit_prophet_ww_all <- function(cut_date, data, state) {
   model = add_regressor(model, 'sludge_pct_transformed')
   model = fit.prophet(model, train_data)
   n = n_distinct(data %>% filter(date >= cut_date) %>% pull(date))
-  future <- make_future_dataframe(model, periods = n, freq = "week") %>% 
+  future <- make_future_dataframe(model, periods = n, freq = "week") %>%
     inner_join(data %>% select(ds = date, raw_pct_transformed, postgrit_pct_transformed,
                                sludge_pct_transformed))
-  return(fit_prophet_reg_end(model, future, train_data, cut_date, state))
+  return(fit_prophet_reg_end(model, future, train_data, cut_date, state, data))
 }
 
 fit_prophet_regressors <- function(cut_date, data, state) {
@@ -137,30 +138,30 @@ fit_prophet_regressors <- function(cut_date, data, state) {
   model = add_regressor(model, 'sludge_pct_transformed')
   model = fit.prophet(model, train_data)
   n = n_distinct(data %>% filter(date >= cut_date) %>% pull(date))
-  future <- make_future_dataframe(model, periods = n, freq = "week") %>% 
-    inner_join(data %>% select(ds = date, ed_transformed, count_transformed, 
-                               percent_transformed, raw_pct_transformed, 
+  future <- make_future_dataframe(model, periods = n, freq = "week") %>%
+    inner_join(data %>% select(ds = date, ed_transformed, count_transformed,
+                               percent_transformed, raw_pct_transformed,
                                postgrit_pct_transformed,
                                sludge_pct_transformed)) #
-  return(fit_prophet_reg_end(model, future, train_data, cut_date, state))
+  return(fit_prophet_reg_end(model, future, train_data, cut_date, state, data))
 }
 
 # Pull in Hospitalization Data --------------------------------------------
-hospitalizations_age <<- read.csv("https://data.cdc.gov/resource/aemt-mg7g.csv?$limit=500000") %>% 
-  dplyr::select(date = week_end_date, state = jurisdiction, weekly_actual_days_reporting_any_data, weekly_percent_days_reporting_any_data, 
-         total_admissions_all_covid_confirmed, total_admissions_adult_covid_confirmed, total_admissions_pediatric_covid_confirmed, 
+hospitalizations_age <<- read.csv("https://data.cdc.gov/resource/aemt-mg7g.csv?$limit=500000") %>%
+  dplyr::select(date = week_end_date, state = jurisdiction, weekly_actual_days_reporting_any_data, weekly_percent_days_reporting_any_data,
+         total_admissions_all_covid_confirmed, total_admissions_adult_covid_confirmed, total_admissions_pediatric_covid_confirmed,
          avg_admissions_all_covid_confirmed, percent_adult_covid_admissions,
          num_hospitals_admissions_all_covid_confirmed) %>%
-  mutate(date = as.Date(date), 
-         mmwr = MMWRweek::MMWRweek(date), 
-         mmwr_date = paste0(mmwr$MMWRyear, '-', if_else(nchar(mmwr$MMWRweek) == 1, 
-                                                        paste0("0", mmwr$MMWRweek), 
+  mutate(date = as.Date(date),
+         mmwr = MMWRweek::MMWRweek(date),
+         mmwr_date = paste0(mmwr$MMWRyear, '-', if_else(nchar(mmwr$MMWRweek) == 1,
+                                                        paste0("0", mmwr$MMWRweek),
                                                         as.character(mmwr$MMWRweek)))) %>%
-  filter(date <= "2024-05-01") # Filter out optional reporting period 
+  filter(date <= "2024-05-01") # Filter out optional reporting period
 
 # Set Prediction Horizons -------------------------------------------------
 max_date <- max(hospitalizations_age$date)
-prediction_horizons <- c(max_date %m-% months(3), max_date %m-% months(6), 
+prediction_horizons <- c(max_date %m-% months(3), max_date %m-% months(6),
                          max_date %m-% months(9), max_date %m-% months(12)) %>%
   as.character()
 
@@ -188,32 +189,32 @@ write_rds(df_list, "prophet_new_horizons_boxcox.rds")
 # Plot
 for(predict_cut in prediction_horizons){
   p <- df_list %>%
-    filter(cut_date == predict_cut, 
+    filter(cut_date == predict_cut,
            date >= "2022-07-01") %>%
     mutate(fitted = exp(fitted) - 1,
            hosp = exp(hosp_transformed) - 1) %>%
     rename(`Fitted Period` = fitted, `Forecast Period` = `Point Forecast`) %>%
     pivot_longer(`Fitted Period`:`Forecast Period`) %>%
     drop_na() %>%
-    filter(cut_date == as.Date(predict_cut), 
+    filter(cut_date == as.Date(predict_cut),
            date >= "2022-07-01") %>%
-    ggplot(aes(x = date)) + 
+    ggplot(aes(x = date)) +
     geom_line(aes(y = hosp, color = "Reported \nHospitalizations")) +
-    geom_point(aes(y = value, color = name), size = 0.5) + 
-    theme_minimal() + 
-    scale_color_manual(values = c("#1B9E77", "#7570B3", "#000000")) + 
+    geom_point(aes(y = value, color = name), size = 0.5) +
+    theme_minimal() +
+    scale_color_manual(values = c("#1B9E77", "#7570B3", "#000000")) +
     labs(y = "Hospitalizations", x = "Date", color = "") +
     facet_wrap(state ~ ., ncol =  4, scales = "free_y")
-  ggsave(paste0("Prophet Results for ", as.Date(predict_cut), ".pdf"), 
-         plot = p, width = 16, height = 10, units = "in", bg = "white", 
+  ggsave(paste0("Prophet Results for ", as.Date(predict_cut), ".pdf"),
+         plot = p, width = 16, height = 10, units = "in", bg = "white",
          dpi = "retina")
 }
 
 # Multivariate Prophet Model ----------------------------------------------
 df_list <- tibble()
 
-full_data <- read_csv('data/updated_joined_df.csv')
-ccf = read_csv('results/ccf.csv') 
+full_data <- read_csv('data/joined_df.csv')
+ccf = read_csv('results/ccf.csv')
 # Run model for each state
 for(state_nm in c(state.abb, "DC")){
   print(state_nm)
@@ -223,43 +224,43 @@ for(state_nm in c(state.abb, "DC")){
     filter(state == state_nm,
            date <= '2024-05-01') %>%
     janitor::clean_names() %>%
-    dplyr::select(date, hosp = total_admissions_all_covid_confirmed, count_pos, 
-                  percent_pos, percent_visits_covid, 
+    dplyr::select(date, hosp = total_admissions_all_covid_confirmed, count_pos,
+                  percent_pos, percent_visits_covid,
                   weighted_raw_ww = avg_detect_prop_weighted_raw_wastewater,
                   weighted_postgrit_ww = avg_detect_prop_weighted_post_grit_removal,
                   weighted_sludge_ww = avg_detect_prop_weighted_primary_sludge) %>%
     # use box-cox transform on hospital data to avoid negatives in predictions
     # we assume box-cox order 0 (so log() transform, but add 1 to avoid fitting
     # log(0) in our model)
-    mutate(count_pos = lag(replace_na(count_pos, 0), 
-                           n = ccf_state %>% 
+    mutate(count_pos = lag(replace_na(count_pos, 0),
+                           n = ccf_state %>%
                              filter(type == 'Count Positive') %>%
-                             pull(lag) %>% abs()), 
+                             pull(lag) %>% abs()),
            count_transformed = log(count_pos+1),
-           percent_pos = lag(replace_na(percent_pos, 0), 
-                             n = ccf_state %>% 
+           percent_pos = lag(replace_na(percent_pos, 0),
+                             n = ccf_state %>%
                                filter(type == '% Positive') %>%
                                pull(lag) %>% abs()),
            percent_transformed = log(percent_pos+1),
-           percent_visits_covid = lag(replace_na(percent_visits_covid, 0), 
-                                      n = ccf_state %>% 
+           percent_visits_covid = lag(replace_na(percent_visits_covid, 0),
+                                      n = ccf_state %>%
                                         filter(type == 'ED Visit') %>%
                                         pull(lag) %>% abs()),
            ed_transformed = log(percent_visits_covid + 1),
-           weighted_raw_ww = lag(replace_na(weighted_raw_ww, 0), 
-                                 n = ccf_state %>% 
+           weighted_raw_ww = lag(replace_na(weighted_raw_ww, 0),
+                                 n = ccf_state %>%
                                    filter(type == 'Weighted WW Raw % Detect') %>%
                                    pull(lag) %>% abs()),
            raw_pct_transformed = log(weighted_raw_ww + 1),
-           weighted_postgrit_ww = lag(replace_na(weighted_postgrit_ww, 0), 
-                                      n = ccf_state %>% 
+           weighted_postgrit_ww = lag(replace_na(weighted_postgrit_ww, 0),
+                                      n = ccf_state %>%
                                         filter(type == 'Weighted WW Post-Grit Removal % Detect') %>%
-                                        pull(lag) %>% abs()), 
+                                        pull(lag) %>% abs()),
            postgrit_pct_transformed = log(weighted_postgrit_ww + 1),
-           weighted_sludge_ww = lag(replace_na(weighted_sludge_ww, 0), 
-                                    n = ccf_state %>% 
+           weighted_sludge_ww = lag(replace_na(weighted_sludge_ww, 0),
+                                    n = ccf_state %>%
                                       filter(type == 'Weighted WW Primary Sludge % Detect') %>%
-                                      pull(lag) %>% abs()), 
+                                      pull(lag) %>% abs()),
            sludge_pct_transformed = log(weighted_sludge_ww + 1),
            hosp_transformed = log(hosp+1)) %>%
     filter(           date >= '2022-10-01') %>%
@@ -279,7 +280,7 @@ for(state_nm in c(state.abb, "DC")){
       bind_rows(ed_count_reg) %>%
       bind_rows(ww_reg) %>%
       bind_rows(ww_all_reg) %>%
-      bind_rows(all_reg) 
+      bind_rows(all_reg)
   }
 }
 
@@ -290,27 +291,27 @@ write_rds(df_list, "results/Multivariate/prophet_final.rds")
 for(predict_cut in prediction_horizons){
   for(type in unique(df_list$model_type)){
     p <- df_list %>%
-      filter(cut_date == predict_cut, 
+      filter(cut_date == predict_cut,
              model_type == type) %>%
       mutate(fitted = exp(fitted) - 1,
              hosp = exp(hosp_transformed) - 1) %>%
-      select(state, cut_date, date, hosp, hosp_transformed, 
+      select(state, cut_date, date, hosp, hosp_transformed,
              fitted, `Point Forecast`, model_type) %>%
       rename(`Fitted Period` = fitted, `Forecast Period` = `Point Forecast`) %>%
       pivot_longer(`Fitted Period`:`Forecast Period`) %>%
       drop_na() %>%
-      filter(cut_date == as.Date(predict_cut), 
+      filter(cut_date == as.Date(predict_cut),
              date >= "2022-07-01") %>%
-      ggplot(aes(x = date)) + 
+      ggplot(aes(x = date)) +
       geom_line(aes(y = hosp, color = "Reported \nHospitalizations")) +
-      geom_point(aes(y = value, color = name), size = 0.5) + 
-      theme_minimal() + 
-      scale_color_manual(values = c("#1B9E77", "#7570B3", "#000000")) + 
+      geom_point(aes(y = value, color = name), size = 0.5) +
+      theme_minimal() +
+      scale_color_manual(values = c("#1B9E77", "#7570B3", "#000000")) +
       labs(y = "Hospitalizations", x = "Date", color = "") +
       facet_wrap(state ~ ., ncol =  4, scales = "free_y")
-    ggsave(paste0("results/Multivariate/Multivariate Plots/Prophet Regressor Results for ", as.Date(predict_cut), "_", 
-                  gsub("%", "PCT", type, fixed = T), ".pdf"), 
-           plot = p, width = 16, height = 10, units = "in", bg = "white", 
+    ggsave(paste0("results/Multivariate/Multivariate Plots/Prophet Regressor Results for ", as.Date(predict_cut), "_",
+                  gsub("%", "PCT", type, fixed = T), ".pdf"),
+           plot = p, width = 16, height = 10, units = "in", bg = "white",
            dpi = "retina")
   }
 }
